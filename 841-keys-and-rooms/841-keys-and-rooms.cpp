95c661159e8879cb23a61d94c16750560571c2ca
Mohamed Riyas <rockingriyas97@yahoo.com>
2022-02-19 17:41:33 +0800

    Time: 11 ms (69.60%), Space: 10.3 MB (80.03%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     void dfs(int rno, int& numv, vector<vector<int>>& rooms, 
             vector<bool>& visited) {
         if (visited[rno]) {
@@ -15,7 +16,39 @@ public:
         return;
     }
     
+    */
+    
+    bool dfs(vector<vector<int>>& rooms) {
+        vector<bool> visited(rooms.size(), false);
+        stack<int> st;
+        st.push(0);
+        visited[0] = true;
+        while(!st.empty()) {
+            int r = st.top();
+            st.pop();
+            //visited[room] = true;
+            //cout << r;
+            for (int i = 0; i < rooms[r].size(); i++) {
+                if (!visited[rooms[r][i]]) {
+                    //cout << rooms[r][i] << "l ";
+                    visited[rooms[r][i]] = true;
+                    st.push(rooms[r][i]);
+                }
+            }
+        }
+        
+        for (int i = 0; i < visited.size(); i++) {
+            if (!visited[i]) {
+                cout << i;
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        /* Approach 1
         int numv = 0;
         //cout << rooms.size();
         vector<bool> visited(rooms.size(), false);
@@ -24,5 +57,9 @@ public:
             return true;
         }
         return false;
+        */
+        
+        // Approach 2 (using stack)
+        return dfs(rooms);
     }
 };
