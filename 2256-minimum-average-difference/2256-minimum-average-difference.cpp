f01cd43691dec6c459288bf6549b46528226211d
Mohamed Riyas <rockingriyas97@yahoo.com>
2022-12-04 16:33:18 +0800

    Time: 331 ms (32.55%), Space: 78.3 MB (96.38%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-        vector<long long> vals(nums.size(), 0);
         long long sum = 0;
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
-            vals[i] = sum;
         }
         int avg = INT_MAX;
         int ind = -1;
+        long long sofar = 0;
         for (int i = 0; i < nums.size(); i++) {
+            sofar += nums[i];
             int calc;
             if (i == nums.size()-1) {
-                calc = (vals[i] / (i+1));
+                calc = (sofar / (i+1));
             } else {
-                calc = (vals[i] / (i+1)) - (vals[nums.size()-1] - vals[i]) / (nums.size()-i-1);
+                calc = (sofar / (i+1)) - (sum - sofar) / (nums.size()-i-1);
             }   
             calc = abs(calc);
             //cout << i << " " << calc << "\n";
